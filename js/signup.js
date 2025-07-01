0b20ea284b23d7116759994e8a8d5b501ea6fbfa
cubalibre59 <cubalibre59@users.noreply.github.com>
2025-07-01 08:59:53 +0200

    dinamyser les formulaire

---
+++ js/signup.js
@@ -0,0 +1,33 @@
+// Sélection des inputs
+
+const inputNom=document.getElementById("NomInput");
+const inputPrenom = document.getElementById("PrenomInput");
+const inputEmail = document.getElementById("emailInput");
+const inputPassword = document.getElementById("PasswordInput");
+
+    // Ajouter les écouteurs pour chaque champ
+inputNom.addEventListener("keyup", () => validateRequired(inputNom));
+inputPrenom.addEventListener("keyup", () => validateRequired(inputPrenom));
+inputEmail.addEventListener("keyup", () => validateRequired(inputEmail));
+inputPassword.addEventListener("keyup", () => validateRequired(inputPassword));
+
+
+
+
+// Fonction de validation
+
+function validateRequired(input){
+
+
+
+
+  
+if (input.value.trim() !='='){
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+}
+else{
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+}
+}
